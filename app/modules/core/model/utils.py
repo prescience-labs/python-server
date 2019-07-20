05d681144947bc49075930f8c0b83b4ba663fc65
smithnlp <smithnlp@users.noreply.github.com>
2019-07-20 14:57:59 -0600

    use spacy for all preprocessing

--- app/modules/core/model/utils.py
+++ app/modules/core/model/utils.py
@@ -1,9 +1,34 @@
+import re
+
 import nltk
-from nltk.tokenize import word_tokenize
-nltk.download('punkt')
+from nltk.tokenize import sent_tokenize
+
+import spacy
+
+
+def doc2dict(doc):
+    """
+    """
+    info = []
+    for token in doc:
+        info.append({'token': token.text,
+                     'lemma': token.lemma_,
+                     'pos': token.pos_,
+                     'dep': token.dep_,
+                     'stop': token.is_stop})
+
+    return info
 
 
 def preprocess(input: str) -> list:
     """
     """
-    return word_tokenize(input)
+    nlp = spacy.load("en_core_web_sm")
+    sents = sent_tokenize(input)
+    sentences = {}
+    for i, s in enumerate(sents):
+        doc = nlp(s)
+        info = doc2dict(doc)
+        sentences[f'sentence_{i}'] = (s, info)
+
+    return sentences
