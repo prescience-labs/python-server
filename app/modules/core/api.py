c54a4db98832ef94bb217f49ca149f901a31a80f
smithnlp <smithnlp@users.noreply.github.com>
2019-07-20 14:58:47 -0600

    change tester sentence

--- app/modules/core/api.py
+++ app/modules/core/api.py
@@ -9,7 +9,7 @@ mod_core = Blueprint('core', __name__, url_prefix='/')
 def core():
     """HTTP root
     """
-    input = "Doctor Jenkins was a complete idiot but I love his staff..."
+    input = "I really love the fit of this blouse, but the color is different than it seems online. The texture is excellent and it's comfortable to wear."
     result = process_single(input)
     return {
         'core': result,
