e9572d9442201c0eabd965f0802864fd69e4445e
smithnlp <smithnlp@users.noreply.github.com>
2019-09-03 16:29:05 +0100

    modify endpoint labels

--- app/modules/core/api.py
+++ app/modules/core/api.py
@@ -5,12 +5,20 @@ from .model.core import process_single
 mod_core = Blueprint('core', __name__, url_prefix='/')
 
 
-@mod_core.route('/')
+@mod_core.route('/infer')
 def core():
-    """HTTP root
+    """HTTP infer endpoint
     """
     input = "I really love the fit of this blouse, but the color is different than it seems online. The texture is excellent and it's comfortable to wear."
     result = process_single(input)
     return {
         'core': result,
     }
+
+
+@mod_core.route('/train')
+def train():
+    """HTTP train model endpoint
+    """
+    result = train_model()
+    return result
