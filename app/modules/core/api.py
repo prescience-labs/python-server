85a81f8186fb2a7cd6ffb8302d9a3b36b8a59f97
smithnlp <smithnlp@users.noreply.github.com>
2019-09-28 21:13:18 +0100

    final comments by bps

--- app/modules/core/api.py
+++ app/modules/core/api.py
@@ -9,9 +9,9 @@ mod_core = Blueprint('core', __name__, url_prefix='/')
 def core():
     """HTTP infer endpoint
     """
-    # input = "I really love the fit of this blouse, but the color is different than it seems online. The texture is excellent and it's comfortable to wear."
+    # change this so it takes an input string from the API request
+    # validate encoding, formatting?
     input = "Fantastic experience! If I was able to put 10 stars I would. I loved my experience it was really well done. The dogs were so cute and happy. I thought the staff were very knowledgeable they certainly know there stuff and the dogs really well. I loved how happy they were sitting on my lap. ( the dogs not the staff )    The tea and cake were delicious and made my experience perfect. What a great atmosphere and experience to have. "
-    # input = ""
     result = process_single(input)
     return {
         'core': result,
@@ -22,5 +22,4 @@ def core():
 def train():
     """HTTP train model endpoint
     """
-    result = train_model()
-    return result
+    pass
